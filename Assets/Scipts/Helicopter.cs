ad1935485058c55489e98bff43371f388d7f7aa0
Fred Riopel <frederic.r.poissant@gmail.com>
2017-08-11 15:06:16 -0400

    Main Project

---
+++ Assets/Scipts/Helicopter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Helicopter : MonoBehaviour {
+
+    private bool called = false;
+    private Rigidbody rigidBody;
+
+	// Use this for initialization
+	void Start () {
+        rigidBody = GetComponent<Rigidbody>();
+	}
+
+    void OnDispatchHelicopter()
+    { 
+            Debug.Log("Helicoper called");
+            rigidBody.velocity = new Vector3(0, 0, 50f);
+            called = true;
+    }
+}
---
+++ Assets/Scipts/RadioSystem.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RadioSystem : MonoBehaviour {
+
+    public AudioClip initialHeliCall;
+    public AudioClip initialCallReply;
+
+    private AudioSource audioSource;
+
+	// Use this for initialization
+	void Start ()
+    {
+        audioSource = GetComponent<AudioSource>();	
+	}
+	
+	void OnMakeInitialHeliCall()
+    {
+        print(name + "OnMakeInitialHeliCall");
+        audioSource.clip = initialHeliCall;
+        audioSource.Play();
+
+        Invoke("OnInitialCallReply", initialHeliCall.length + 1f);
+    }
+
+    void OnInitialCallReply()
+    {
+        print(name + "OnInitialCallReply");
+        audioSource.clip = initialCallReply;
+        audioSource.Play();
+
+        BroadcastMessage("OnDispatchHelicopter");
+    }
+}
